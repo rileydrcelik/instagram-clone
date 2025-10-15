76ead76238b412fe8678704104a2b758131eb5dd
rileydrcelik <rileydrcelik@gmail.com>
2025-10-14 21:15:31 -0400

    api key leak bruh

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -4,13 +4,13 @@ import {getFirestore} from "firebase/firestore";
 import {getStorage} from "firebase/storage"
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCcD91HZIRdphH2_VZ0WV6kgjPPMy3R558",
-  authDomain: "insta-clone-b8ccf.firebaseapp.com",
-  projectId: "insta-clone-b8ccf",
-  storageBucket: "insta-clone-b8ccf.firebasestorage.app",
-  messagingSenderId: "331355228455",
-  appId: "1:331355228455:web:3f9e321236ac2c53834102",
-  measurementId: "G-8VRX13RT2Y"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
